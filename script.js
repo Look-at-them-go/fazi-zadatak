5f5ac258e7e5c1f774b8d5cac8afd81e2e5adf63
Dragan Djordjevic <GaGzO78@gmail.com>
2023-12-13 06:45:20 +0100

    functionality implemented

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.onload=function(){
+
 const form = document.querySelector('form');
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -7,11 +9,21 @@ form.addEventListener('submit', (e) => {
     const obj = Object.fromEntries(formData.entries());
     const img = convertBase64(obj.image);
     if(Users.length < 10){
-        Users.push(createUser(firstCharacterUpper(obj.Firstname),
-                            firstCharacterUpper(obj.Surname),
-                            img));
-    } else {alert("You can have no more than 10 users")}
+        var user = createUser(firstCharacterUpper(obj.Firstname),firstCharacterUpper(obj.Surname),img);
+        Users.push(user);
+        
+        var userCanvas = document.createElement("canvas");
 
+        userCanvas.setAttribute("id",user.id);
+        userCanvas.setAttribute("width",679);
+        userCanvas.setAttribute("height",382)
+
+        var wrapper = document.getElementById("canv");
+        wrapper.appendChild(userCanvas);
+
+        drawCard(user.id,user)
+
+    } else {alert("You can have no more than 10 users")}
     console.log(Users);
 });
 }
@@ -80,5 +92,40 @@ async function convertBase64(image){
     });
     var image = await promise;
     return image;
+}
+
+async function drawCard(id,user){
+    var c = document.getElementById(id);
+    var ctx = c.getContext("2d");
+
+
+    // inital rectangles
+    ctx.fillStyle = `rgb(6,19,54)`;
+    ctx.fillRect(0, 0, 679, 382);
+
+    ctx.fillStyle = `rgb(237,28,36)`;
+    ctx.fillRect(0, 314, 679, 39);
+
+    ctx.fillStyle = `rgb(255,255,255)`;
+    ctx.fillRect(0, 352, 679, 30);
+
+    // draws the image
+    var image = new Image();
+    image.onload = function() {
+    ctx.drawImage(image, 46, 34,196,248);
+    };
+    image.src = await user.image;
+
+    // draws the name and surname
+    ctx.font = "26px Open Sans";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText(user.firstname + " " + user.surname, c.width*0.63 , c.height*0.65 );
+
+    //draws the id
+    ctx.font = "26px Open Sans";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText("ID " + user.id, 590 , 342 );
 }
 
